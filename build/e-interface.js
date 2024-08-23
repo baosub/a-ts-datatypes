c188dbb621ee6bdd6895cd4d6784c8ec25930b16
Baldo <barrosoub@protonmail.com>
2024-08-23 11:16:32 -0600

    interfaces were added

---
+++ build/e-interface.js
@@ -0,0 +1,6 @@
+"use strict";
+const myCar = {
+    brand: "Toyota",
+    year: 2020
+};
+console.log(myCar);
---
+++ e-interface.ts
@@ -0,0 +1,11 @@
+// Definiendo una interfaz para un objeto
+interface Car {
+    brand: string;
+    year: number;
+}
+
+const myCar: Car = {
+    brand: "Toyota",
+    year: 2020
+};
+console.log(myCar);
