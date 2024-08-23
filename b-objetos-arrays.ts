ff6c32c2634da19caf74437d24453633f08f2207
Baldo <barrosoub@protonmail.com>
2024-08-23 10:44:31 -0600

    object and array types were added

---
+++ b-objetos-arrays.ts
@@ -0,0 +1,21 @@
+// Tipo: objeto
+const person: { name: string; age: number } = {
+    name: "Baub",
+    age: 35
+};
+console.log(person);
+
+// Tipo: array de números
+const numbers: number[] = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// Tipo: array de strings
+const fruits: string[] = ["Apple", "Banana", "Orange"];
+console.log(fruits);
+
+// Tipo: array de objetos
+const people: { name: string; age: number }[] = [
+    { name: "Baub", age: 35 },
+    { name: "Alice", age: 28 }
+];
+console.log(people);
---
+++ build/b-objetos-arrays.js
@@ -0,0 +1,15 @@
+"use strict";
+const person = {
+    name: "Baub",
+    age: 35
+};
+console.log(person);
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+const fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits);
+const people = [
+    { name: "Baub", age: 35 },
+    { name: "Alice", age: 28 }
+];
+console.log(people);
