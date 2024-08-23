a1813aa8beb87d4eb37397ce00c2a4bbad45aad8
Baldo <barrosoub@protonmail.com>
2024-08-23 17:53:19 -0600

    some modifications

--- g-aliases.ts
+++ g-aliases.ts
@@ -1,5 +1,5 @@
 // Definimos un alias de tipo para un objeto que representa a una persona
-//PAra definir tipos de datos, primitivos o avanzados.
+//PAra definir tipos de datos, primitivos o
 
 type Person = {
     name: string;
@@ -18,4 +18,9 @@ type Person = {
   // Imprimimos las propiedades individuales
   console.log(`Nombre: ${person1.name}`);
   console.log(`Edad: ${person1.age}`);
+
+  console.log(person1.name);
+  console.log(person1.name);
+  
+  
   
