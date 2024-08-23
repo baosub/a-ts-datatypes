4eb1f68ac1d16b2336d8c386278fbc6c79670281
Baldo <barrosoub@protonmail.com>
2024-08-23 10:36:29 -0600

    the primitive types were dded

---
+++ a-primitive-types.ts
@@ -0,0 +1,27 @@
+// Tipo: number
+const age: number = 25;
+console.log(age);
+
+// Tipo: string
+const nameMain: string = "Baub";
+console.log(nameMain);
+
+// Tipo: boolean
+const isStudent: boolean = true;
+console.log(isStudent);
+
+// Tipo: null
+const emptyValue: null = null;
+console.log(emptyValue);
+
+// Tipo: undefined
+let notDefined: undefined = undefined;
+console.log(notDefined);
+
+// Tipo: any (puede ser cualquier tipo)
+let randomValue: any = "Puede ser cualquier cosa";
+console.log(randomValue);
+
+// Cambiar el valor de any a otro tipo
+randomValue = 42;
+console.log(randomValue);
---
+++ build/a-primitive-types.js
@@ -0,0 +1,22 @@
+"use strict";
+// Tipo: number
+const age = 25;
+console.log(age);
+// Tipo: string
+const nameMain = "Baub";
+console.log(nameMain);
+// Tipo: boolean
+const isStudent = true;
+console.log(isStudent);
+// Tipo: null
+const emptyValue = null;
+console.log(emptyValue);
+// Tipo: undefined
+let notDefined = undefined;
+console.log(notDefined);
+// Tipo: any (puede ser cualquier tipo)
+let randomValue = "Puede ser cualquier cosa";
+console.log(randomValue);
+// Cambiar el valor de any a otro tipo
+randomValue = 42;
+console.log(randomValue);
